e66ad1a04c72fd748aac5512e5c6356f1cfff4b6
Nick <33524818+Nikhil-KO@users.noreply.github.com>
2021-01-03 12:35:19 +0000

    refactor client tests so its easier to follow, using globals

--- clientTest.py
+++ clientTest.py
@@ -8,9 +8,7 @@ import unittest
 import threading
 from client.observer import Observer
 
-test_completed = False
-SLEEP_CONSTANT = 2
-letters = string.ascii_lowercase
+# TODO make tests appear in separate folder
 
 class FakeHandler:
 
@@ -32,113 +30,85 @@ class FakeHandler:
     def on_delete(self, path : pathlib.Path, directory : bool):
         self.deleted = path
 
+test_completed : bool = False
+SLEEP_CONSTANT : float = 2
+fake_handler : FakeHandler = None
+root_dir : pathlib.Path = None
+letters = string.ascii_lowercase
+
 class ClientUnitTests(unittest.TestCase):
     
-    def __init__(self, methodName: str, handle : FakeHandler, root_dir : pathlib.Path) -> None:
-        super().__init__(methodName=methodName)
-        self.handle = handle
-        self.root_dir = root_dir
-
     def test_create_dir(self):
         folder_name = ''.join(random.choice(letters) for i in range(10))
         path = pathlib.Path(folder_name)
-        test_folder = os.path.join(self.root_dir, path)
+        test_folder = os.path.join(root_dir, path)
         os.makedirs(test_folder)
         time.sleep(2 * SLEEP_CONSTANT)
-        self.assertEqual(str(self.handle.created), str(path), "Observer did not pick up directory being made")
+        self.assertEqual(str(fake_handler.created), str(path), "Observer did not pick up directory being made")
+        return path
 
     def test_create_file(self):
         file_name = ''.join(random.choice(letters) for i in range(10)) + ".txt"
         path = pathlib.Path(file_name)
-        test_file = os.path.join(self.root_dir, path)
+        test_file = os.path.join(root_dir, path)
         with open(test_file, 'w') as f:
             print("unit test file", file=f)
         time.sleep(2 * SLEEP_CONSTANT)
-        self.assertEqual(str(self.handle.created), str(path), "Observer did not pick up file being made")
+        self.assertEqual(str(fake_handler.created), str(path), "Observer did not pick up file being made")
+        return path
 
     def test_modify_file(self):
-        file_name = ''.join(random.choice(letters) for i in range(10)) + ".txt"
-        path = pathlib.Path(file_name)
-        test_file = os.path.join(self.root_dir, path)
-        with open(test_file, 'w') as f:
-            print("unit test file", file=f)
-        time.sleep(2 * SLEEP_CONSTANT)
-        self.assertEqual(str(self.handle.created), str(path), "Observer did not pick up file being made")
+        path = self.test_create_file()
+        test_file = os.path.join(root_dir, path)
         # modify contents
         with open(test_file, 'w') as f:
             print("modifed text", file=f)
         time.sleep(2 * SLEEP_CONSTANT)
-        self.assertEqual(str(self.handle.modified), str(path), "Observer did not pick up file being modified")
+        self.assertEqual(str(fake_handler.modified), str(path), "Observer did not pick up file being modified")
 
     def test_move_dir(self):
-        folder_name = ''.join(random.choice(letters) for i in range(10))
-        path = pathlib.Path(folder_name)
-        test_folder = os.path.join(self.root_dir, path)
-        os.makedirs(test_folder)
-        time.sleep(2 * SLEEP_CONSTANT)
-        self.assertEqual(str(self.handle.created), str(path), "Observer did not pick up directory being made")
-        new_folder_name =  ''.join(random.choice(letters) for i in range(10))
-        new_test_folder = os.path.join(self.root_dir, new_folder_name)
+        path = self.test_create_dir()
+        test_folder = os.path.join(root_dir, path)
         # move folder
+        new_folder_name =  ''.join(random.choice(letters) for i in range(10))
+        new_test_folder = os.path.join(root_dir, new_folder_name)
         shutil.move(test_folder, new_test_folder)
         time.sleep(2 * SLEEP_CONSTANT)
-        self.assertIsNotNone(self.handle.moved, "Observer did not detect folder move")
-        self.assertTupleEqual((str(path), str(new_folder_name)), (str(self.handle.moved[0]), str(self.handle.moved[1])), "Observer did not detect folder move")
+        self.assertIsNotNone(fake_handler.moved, "Observer did not detect folder move")
+        self.assertTupleEqual((str(path), str(new_folder_name)), (str(fake_handler.moved[0]), str(fake_handler.moved[1])), "Observer did not detect folder move")
 
     def test_move_file(self):
-        file_name = ''.join(random.choice(letters) for i in range(10)) + ".txt"
-        path = pathlib.Path(file_name)
-        test_file = os.path.join(self.root_dir, path)
-        with open(test_file, 'w') as f:
-            print("unit test file", file=f)
-        time.sleep(2 * SLEEP_CONSTANT)
-        self.assertEqual(str(self.handle.created), str(path), "Observer did not pick up file being made")
+        path = self.test_create_file()
+        test_file = os.path.join(root_dir, path)
         # move file
         new_file_name = ''.join(random.choice(letters) for i in range(10)) + ".txt"
         new_path = pathlib.Path(new_file_name)
-        new_test_file = os.path.join(self.root_dir, new_path)
+        new_test_file = os.path.join(root_dir, new_path)
         shutil.move(test_file, new_test_file)
         time.sleep(2 * SLEEP_CONSTANT)
-        self.assertIsNotNone(self.handle.moved, "Observer did not detect file move")
-        self.assertTupleEqual((path, new_path), (self.handle.moved[0], self.handle.moved[1]), "Observer did not detect file move")
+        self.assertIsNotNone(fake_handler.moved, "Observer did not detect file move")
+        self.assertTupleEqual((path, new_path), (fake_handler.moved[0], fake_handler.moved[1]), "Observer did not detect file move")
 
     def delete_dir(self):
-        folder_name = ''.join(random.choice(letters) for i in range(10))
-        path = pathlib.Path(folder_name)
-        test_folder = os.path.join(self.root_dir, path)
-        os.makedirs(test_folder)
-        time.sleep(2 * SLEEP_CONSTANT)
-        self.assertEqual(str(self.handle.created), str(path), "Observer did not pick up directory being made")
+        path = self.test_create_dir()
+        test_folder = os.path.join(root_dir, path)
         # delete folder
         shutil.rmtree(test_folder)
         time.sleep(2 * SLEEP_CONSTANT)
-        self.assertEqual(str(self.handle.deleted), str(folder_name), "Observer did not detect folder deletion")
+        self.assertEqual(str(fake_handler.deleted), str(path), "Observer did not detect folder deletion")
 
     def delete_file(self):
-        file_name = ''.join(random.choice(letters) for i in range(10)) + ".txt"
-        path = pathlib.Path(file_name)
-        test_file = os.path.join(self.root_dir, path)
-        with open(test_file, 'w') as f:
-            print("unit test file", file=f)
-        time.sleep(2 * SLEEP_CONSTANT)
-        self.assertEqual(str(self.handle.created), str(path), "Observer did not pick up file being made")
+        path = self.test_create_file()
+        test_file = os.path.join(root_dir, path)
         # delete file
         os.remove(test_file)
         time.sleep(2 * SLEEP_CONSTANT)
-        self.assertEqual(str(self.handle.deleted), str(path), "Observer did not detect file deletion")
+        self.assertEqual(str(fake_handler.deleted), str(path), "Observer did not detect file deletion")
 
-def run_tests(handle : FakeHandler, root_dir : pathlib.Path):
+def run_tests():
     print("running client tests")
     time.sleep(2*SLEEP_CONSTANT) # need to wait for client to set up
-    test_suite = unittest.TestSuite()
-    test_suite.addTest(ClientUnitTests('test_create_dir', handle, root_dir))
-    test_suite.addTest(ClientUnitTests('test_create_file', handle, root_dir))
-    test_suite.addTest(ClientUnitTests('test_modify_file', handle, root_dir))
-    test_suite.addTest(ClientUnitTests('test_move_dir', handle, root_dir))
-    test_suite.addTest(ClientUnitTests('test_move_file', handle, root_dir))
-    test_suite.addTest(ClientUnitTests('delete_dir', handle, root_dir))
-    test_suite.addTest(ClientUnitTests('delete_file', handle, root_dir))
-    unittest.TextTestRunner(verbosity=2).run(test_suite)
+    unittest.main()
     global test_completed
     test_completed = True
 
@@ -148,13 +118,15 @@ def run_observer(observer : Observer):
         time.sleep(SLEEP_CONSTANT)
 
 def run():
+    global root_dir
     root_dir = pathlib.Path('client/listen')
+    global fake_handler
     fake_handler = FakeHandler()
     observer = Observer(root_dir, fake_handler.on_modify, fake_handler.on_move, fake_handler.on_create, fake_handler.on_delete)
     t1 = threading.Thread(target=run_observer, args=(observer,))
     t1.start()
     # REVIEW maybe this doesn't need a need thread to spin up?
-    threading.Thread(target=run_tests, args=(fake_handler, root_dir,)).start()
+    threading.Thread(target=run_tests).start()
     t1.join()
 
 if __name__ == "__main__":
