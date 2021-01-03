c07ad2bf113249e8bea49763c8830470e2c0132b
Nick <33524818+Nikhil-KO@users.noreply.github.com>
2021-01-03 14:15:26 +0000

    kind of hacky test suite

--- testRunner.py
+++ testRunner.py
@@ -1,8 +1,3 @@
-from clientTest import run as clientTests
-from serverTests import run as serverTests
-
 if __name__ == "__main__":
-    # These tests ensure the observer picks up on the required file system changes
-    clientTests()
-    # These tests ensure the functionality of the dropboxService is met
-    serverTests()
+    exec(open('clientTest.py').read())
+    exec(open('serverTest.py').read())
